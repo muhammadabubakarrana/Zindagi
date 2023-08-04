ae2fe02d9bef985915f7922e28812d02a6896031
Muhammad Abubakar <muhammadabubakarrana5@gmail.com>
2023-08-04 21:39:50 +0500

    react hook form

--- android/app/src/main/java/com/Zindagi/com/MainActivity.java
+++ android/app/src/main/java/com/Zindagi/com/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rntemplate0716;
+package com.Zindagi.com;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RnTemplate0716";
+    return "Zindagi";
   }
 @Override
   protected void onCreate(Bundle savedInstanceState) {
--- android/app/src/main/java/com/Zindagi/com/MainApplication.java
+++ android/app/src/main/java/com/Zindagi/com/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rntemplate0716;
+package com.Zindagi.com;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
